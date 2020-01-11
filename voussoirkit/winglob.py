e3f080b74ffa5c12889504c1728ddca4ae48494c
Ethan Dalool <ethan@voussoir.net>
2020-01-11 01:37:33 -0800

    Update winglob module docstring.

--- voussoirkit/winglob.py
+++ voussoirkit/winglob.py
@@ -3,7 +3,8 @@ On Windows, square brackets do not have a special meaning in glob strings.
 However, python's glob module is written for unix-style globs in which brackets
 represent character classes / ranges.
 
-Calling glob.escape would also escape asterisk which may not be desired.
+On Windows we should escape those brackets to get the right results.
+But calling glob.escape would also escape asterisk which may not be desired.
 So this module just provides a modified version of glob.glob which will escape
 only square brackets when called on windows, and behave normally on linux.
 '''
