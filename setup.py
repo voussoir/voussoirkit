9a8e9faf19118624da7eaf8507257ae9efb98197
Ethan Dalool <ethan@voussoir.net>
2020-01-15 22:35:45 -0800

    Bump to version 0.0.39.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='voussoirkit',
     packages=setuptools.find_packages(),
-    version='0.0.38',
+    version='0.0.39',
     author='voussoir',
     author_email='ethan@voussoir.net',
     description='voussoir\'s toolkit',
