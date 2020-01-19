fee0871ef4e0d8b4f70702dca54f1d1f480330b4
Ethan Dalool <ethan@voussoir.net>
2020-01-18 22:41:17 -0800

    Change email in setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     version='0.0.39',
     author='voussoir',
-    author_email='ethan@voussoir.net',
+    author_email='pypi@voussoir.net',
     description='voussoir\'s toolkit',
     url='https://github.com/voussoir/voussoirkit',
     install_requires=['pyperclip', 'pywin32', 'winshell']
