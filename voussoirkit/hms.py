16b1d85dbd02c912af878721deb8d229c3e81f49
Ethan Dalool <ethan@voussoir.net>
2020-07-20 18:04:12 -0700

    Add main function to hms.py.

--- voussoirkit/hms.py
+++ voussoirkit/hms.py
@@ -1,4 +1,5 @@
 import math
+import sys
 
 def hms_to_seconds(hms):
     '''
@@ -34,3 +35,18 @@ def seconds_to_hms(seconds):
     parts.append(seconds)
     hms = ':'.join(f'{part:02d}' for part in parts)
     return hms
+
+def main(args):
+    from voussoirkit import pipeable
+    for line in pipeable.go(args, strip=True, skip_blank=True):
+        if ':' in line:
+            line = hms_to_seconds(line)
+        else:
+            line = float(line)
+            if line > 60:
+                line = seconds_to_hms(line)
+
+        pipeable.output(f'{line}')
+
+if __name__ == '__main__':
+    raise SystemExit(main(sys.argv[1:]))
