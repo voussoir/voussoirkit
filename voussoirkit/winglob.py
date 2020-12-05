f8c0d46c5ef517e10c0ed4276b1444b5fa316915
Ethan Dalool <git@voussoir.net>
2020-12-05 15:07:04 -0800

    Alphabetize these functions.

--- voussoirkit/winglob.py
+++ voussoirkit/winglob.py
@@ -18,8 +18,8 @@ def fix(pattern):
         pattern = re.sub(r'(\[|\])', r'[\1]', pattern)
     return pattern
 
-def glob(pathname, *, recursive=False):
-    return python_glob.glob(fix(pathname), recursive=recursive)
-
 def fnmatch(name, pat):
     return python_fnmatch.fnmatch(name, fix(pat))
+
+def glob(pathname, *, recursive=False):
+    return python_glob.glob(fix(pathname), recursive=recursive)
