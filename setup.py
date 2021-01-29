7f5c29841831ffb279f02babaeb3b78f87257f1e
Ethan Dalool <git@voussoir.net>
2021-01-29 12:15:07 -0800

    Bump to version 0.0.61.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='voussoirkit',
     packages=setuptools.find_packages(),
-    version='0.0.60',
+    version='0.0.61',
     author='voussoir',
     author_email='pypi@voussoir.net',
     description='voussoir\'s toolkit',
