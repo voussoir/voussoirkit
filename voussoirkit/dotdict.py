73e8b4800330d6840735e84e623434fe4823a7c5
Ethan Dalool <git@voussoir.net>
2022-11-07 17:57:40 -0800

    Add dotdict._to_dict.

--- voussoirkit/dotdict.py
+++ voussoirkit/dotdict.py
@@ -28,10 +28,13 @@ class DotDict:
     def __setattr__(self, key, value):
         self.__dict__[key] = value
 
-    def __iter__(self):
+    def _to_dict(self):
         display = self.__dict__.copy()
         display.pop('_DotDict__default')
-        return iter(display.items())
+        return display
+
+    def __iter__(self):
+        return iter(self._to_dict().items())
 
     def __repr__(self):
         display = self.__dict__.copy()
